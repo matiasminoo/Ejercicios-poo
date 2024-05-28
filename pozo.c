44c3c9a46fb226738d77d9ace48574e6d25f07ee
Matias miño <mati.mino2006@gmail.com>
2024-05-28 15:39:19 -0300

    Create pozo.c

---
+++ pozo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void tutorial() {
+	
+}
+
+void pozo() {
+	int map[10][10];
+	
+	
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			
+		}
+	}
+	
+	
+	}
+}
+int main(int argc, char *argv[]) {
+	srand(time[NULL]);
+	
+	
+	
+	
+	return 0;
+}
