b36951b3794f5f22f5a77807ff83a47db7e74c9b
Matias miño <mati.mino2006@gmail.com>
2024-07-03 12:43:08 -0300

    Update prueba.c

--- prueba.c
+++ prueba.c
@@ -1,121 +1,148 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-int main(int argc, char *argv[])
-{
-    int desicion;
-
-    printf("Elegi el ejercicio al que queres acceder: \n");
-    printf("1 \n");
-    printf("2 \n");
-    printf("3 \n");
-    printf("4 \n");
-    printf("5 \n");
-    scanf("%d", &desicion);
-
-//Ejercicio 1
-    int Hentrada;
-    int Hsalida;
-    int precio = 500;
-//Ejercicio 2
-    int num, num2;
+int main() {
+    // Variables para el ejercicio 1
+    string hora_entrada, hora_salida;
+    // Variables para el ejercicio 2
+    double num1, num2;
     int producto;
-//Ejercicio 3
-    int km;
-    int precioC;
-    int h, m;
-    int TotalP;
-//Ejercicio 4
-    int CantidadEntradas;
-    int ValorEntradas;
-//Ejercicio 5
+    // Variables para el ejercicio 4
+    int pago_a_realizar;
+    int cantidad_entradas;
+    int precio_entradas = 300;
+    // Variables para el ejercicio 5
     int personas;
     int total = 0;
 
 
-    switch (desicion) {
-    case 1:
-        printf("Ingresa hora de entrada (en formato militar): \n");
-        scanf("%d", &Hentrada);
-        printf("Ingrese hora de salida (en formato militar): \n");
-        scanf("%d", &Hsalida);
-        
-        
+    int decision;
+    cout << "1. Ejercicio 1" << endl;
+    cout << "2. Ejercicio 2" << endl;
+    cout << "3. Ejercicio 3" << endl;
+    cout << "4. Ejercicio 4" << endl;
+    cout << "5. Ejercicio 5" << endl;
+    cout << "Seleccione el ejercicio al que desea acceder: ";
+    cin >> decision;
 
-        printf("El total a pagar es de: %d", precio);
-    
-        
-        break;
-    case 2:
-        printf("Ingresa dos numeros positivos y enteros: \n");
-        scanf("%d %d", &num, &num2);
+    if (decision < 1 || decision > 5) {
+        cout << "Ejercicio inválido." << endl;
+        return 1;
+    }
 
-        if (num < 0 || num2 < 0) {
-            printf("Ingresa dos numeros POSITIVOS Y ENTEROS: \n");
+    switch(decision) {
+        case 1:
+            // Solicitar hora de entrada y hora de salida
+            cout << "Ingrese la hora de entrada (formato militar, ej. 0830): ";
+            cin >> hora_entrada;
+            cout << "Ingrese la hora de salida (formato militar, ej. 1345): ";
+            cin >> hora_salida;
+
+            // Convertir horas de entrada y salida a minutos desde la medianoche
+            {
+                int entrada_minutos = stoi(hora_entrada.substr(0, 2)) * 60 + stoi(hora_entrada.substr(2));
+                int salida_minutos = stoi(hora_salida.substr(0, 2)) * 60 + stoi(hora_salida.substr(2));
+
+                // Calcular la duración en minutos
+                int duracion_minutos = salida_minutos - entrada_minutos;
+
+                // Calcular el total a pagar
+                int total_pagar;
+                if (duracion_minutos <= 60) {
+                    total_pagar = 500;
+                } else {
+                    // Calcular horas completas adicionales
+                    int horas_completas_adicionales = duracion_minutos / 60;
+                    if (duracion_minutos % 60 != 0) {
+                        horas_completas_adicionales++;
+                    }
+                    total_pagar = 500 + 250 * (horas_completas_adicionales - 1);
+                }
+
+                // Mostrar total a pagar
+                cout << "El total a pagar por el servicio de estacionamiento es: $" << total_pagar << endl;
+            }
+            break;
+
+        case 2:
+            // Solicitar dos números enteros positivos
+            cout << "Ingresa dos números positivos y enteros: ";
+            cin >> num1 >> num2;
+
+            // Verificar que sean positivos
+            if (num1 < 0 || num2 < 0) {
+                cout << "Por favor ingresa dos números POSITIVOS Y ENTEROS." << endl;
+                return 1;
+            }
+            // Verificar que num1 sea entero
+            if (num1 == floor(num1)) {
+                cout << num1 << " es un número entero." << endl;
+        } else {
+                cout << num1 << " no es un número entero." << endl;
             return 1;
         }
-        if (num < num2) {
-        for (int i = 0; i < num2; i++)
-        {
-            producto = num + num2;
-        }
-        }
-        else {
-            for (int i = 0; i < num; i++)
-            {
-                producto = num + num2;
+        // Verificar que num2 sea entero
+             if (num2 == floor(num2)) {
+        cout << num2 << " es un número entero." << endl;
+    } else {
+        cout << num2 << " no es un número entero." << endl;
+        return 1;
+    }
+            // Calcular el producto de num1 por num2 utilizando un bucle
+            producto = 0;
+            for (int i = 0; i < num2; i++) {
+                producto += num1;
             }
-        }
-            printf("El producto es: %d \n", producto);
+
+            cout << "El producto es: " << producto << endl;
             break;
-           
-            
-    case 3:
-        printf("Ingrese la cantidad de km recorridos: \n");
-        scanf("%d", &km);
-        printf("Ingrese el precio por litro del combustible: \n");
-        scanf("%d", &precioC);
-        printf("Ingrese el tiempo que se ha tardado(horas y min): \n");
-        scanf("%d %d", &h, &m);
 
+        case 4:
+            // Pido la cantidad de entradas
+            cout << "Cuantas entradas quiere comprar (Maximo 4): ";
+            cin >> cantidad_entradas;
+            // Verifico que la cantidad de entradas sea mayor a 0 y menor a 4
+            if (cantidad_entradas > 4 || cantidad_entradas < 0) {
+                cout << "Cantidad de entradas invalida (Minimo 1 y maximo 4)." << endl;
+                return 1;
+            }
+            //Realizar los calculos para conseguir el precio del pago a realizar en base a las distintas cantidades de entradas
+            if (cantidad_entradas == 2) {
+                pago_a_realizar = (precio_entradas * 2) * 0.90;
+                cout << "El pago a realizar es de: $" << pago_a_realizar << " pesos." << endl; 
+            } else if (cantidad_entradas == 3) {
+                pago_a_realizar = (precio_entradas * 3) * 0.85;
+                cout << "El pago a realizar es de: $" << pago_a_realizar << " pesos." << endl; 
+            } else if (cantidad_entradas == 4) {
+                pago_a_realizar = (precio_entradas * 4) * 0.80;
+                cout << "El pago a realizar es de: $" << pago_a_realizar << " pesos." << endl; 
+            } else {
+                pago_a_realizar = precio_entradas;
+                cout << "El pago a realizar es de: $" << pago_a_realizar << " pesos." << endl; 
+            }
+            break;
 
-        break;
-    case 4:
-        printf("Ingresa la cantidad de entradas que queres comprar: \n");
-        scanf("%d", &CantidadEntradas);
-
-        //Valor de entradas es de 1000
-
-        if (CantidadEntradas == 2) {
-            ValorEntradas = (1000 * 2) * 0.90;
-            printf("El valor con el descuento aplicado es: %d \n", ValorEntradas);
-        }
-        else if (CantidadEntradas == 3) {
-            ValorEntradas = (1000 * 3) * 0.85;
-            printf("El valor con el descuento aplicado es: %d \n", ValorEntradas);
-        }
-        else {
-            ValorEntradas = (1000 * 4) * 0.80;
-            printf("El valor con el descuento aplicado es: %d \n", ValorEntradas);
-        }
-        break;
-    case 5:
-        printf("Ingresa la cantidad de personas: \n");
-        scanf("%d", &personas);
-
+        case 5:
+        //Pedir la cantidad de personas
+             cout << "Ingresa la cantidad de personas: " << endl;
+             cin >> personas;
+        //Realizar los calculos en base a las distintas situaciones
         if (personas < 200) {
             total = personas * 6500;
-            printf("El valor total es de: %d \n", total);
+            cout << "El valor total es de: " << total << endl;
         }
         else if (personas > 200 && personas <= 300) {
             total = personas * 5500;
-            printf("El valor total es de: %d \n", total);
+            cout << "El valor total es de: " << total << endl;
         }
         else {
             total = personas * 4500;
-            printf("El valor total es de: %d \n", total);
+            cout << "El valor total es de: " << total << endl;
         }
         break;
     }
+
     return 0;
 }
